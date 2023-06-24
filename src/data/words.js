741d02486456c6b67cfd8f7aa070c999aba4f7a8
luizpk <luizpkpinheiro@gmail.com>
2023-06-24 00:48:10 -0300

    Iniciando projeto

---
+++ src/data/words.js
@@ -0,0 +1,6 @@
+export const wordList={
+    carro:["fusca","gol","uno","opala","monza","pajero","corsa","escorte","belina"],
+    escola:["lousa","carteira","cantina","professor","recreio","prova", "recuperação", "colegas"],
+    fruta: ["banana","abacate","melancia","tomate", "morango", "pera"],
+    corpo: ["cabeça","ombro", "joelho", "cotovelo", "nariz", "bochecha", "orelha"],
+};
